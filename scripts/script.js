36c200a4fc44aa0ffd67e8ac2c1103e87b1f8f0f
RobertB512 <rwbcool@gmail.com>
2023-01-05 11:08:32 -0800

    Add hover effect, move styles to styles file

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,10 @@
-// create a grid of 16x16 divs in a parent container (which is already in the html)
 let body = document.querySelector("body")
 let container = document.querySelector(".container")
+let boxCount = 256
 
-body?.setAttribute("style", "height: 100vh;")
-container?.setAttribute("style", "height: 100%; width: 100%; display: flex; flex-wrap: wrap;")
-
-for (let box = 0; box < 256; box++) {
+for (let box = 0; box < boxCount; box++) {
   let child = document.createElement("div")
   child.classList.add("child")
-  child.setAttribute("style", "border: 0.5px solid black; height: 6.25%; width: 6.25%; background-color: white;")
   container?.appendChild(child)
 }
 
